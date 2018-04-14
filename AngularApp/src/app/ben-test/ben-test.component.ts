c0082246339aa1e88775380803be3b1bc6b03dd2
Ben Conaty <ben.conaty@gmail.com>
2018-04-14 15:23:02 -0700

    tinkering with test component

--- AngularApp/src/app/ben-test/ben-test.component.ts
+++ AngularApp/src/app/ben-test/ben-test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {  } from '@types/googlemaps';
 import { HttpService } from '../http.service';
 
@@ -8,10 +8,22 @@ import { HttpService } from '../http.service';
   styleUrls: ['./ben-test.component.css']
 })
 export class BenTestComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any
   map : google.maps.Map;
-  constructor() { }
+  constructor(
+    private _http : HttpService
+  ) { }
 
   ngOnInit() {
+    this.initMap();
   }
 
+  initMap() {
+    const mapProp = {
+      center: new google.maps.LatLng(20, 0),
+      zoom: 2,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
