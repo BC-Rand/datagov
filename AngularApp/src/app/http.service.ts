b24a1be61b15d9c984b1da8ee1ef77203abdaf8a
Matt Krabacher <matt.krabacher@gmail.com>
2018-04-14 19:05:43 -0700

    m

--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -10,4 +10,10 @@ export class HttpService {
         console.log('getting locations in service');
         return this._http.get('/allLocations');
     }
+
+    geocode(address) {
+        console.log('geocoding in service');
+        // tslint:disable-next-line:max-line-length
+        return this._http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyCCXR-DhGmrWqToo99WqiqgT26gabtvvO4');
+    }
 }
