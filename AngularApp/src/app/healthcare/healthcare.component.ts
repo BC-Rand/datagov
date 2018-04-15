5d2cf1554977b254625d40b43ec017795c8f8c50
Lee Watterson <leewatterson@gmail.com>
2018-04-15 14:58:10 -0700

    committing

--- AngularApp/src/app/healthcare/healthcare.component.ts
+++ AngularApp/src/app/healthcare/healthcare.component.ts
@@ -17,7 +17,7 @@ export class HealthcareComponent implements OnInit {
   hospCoordLat: any;
   hospCoordLng: any;
   distance: any;
-
+  distList = [];
 
   constructor(private _httpService: HttpService) { }
 
@@ -31,25 +31,24 @@ export class HealthcareComponent implements OnInit {
     let obs = this._httpService.getLocation(this.street);
     obs.subscribe(data => {
       this.location = (data['results'][0].address_components[4].long_name).slice(0, -7);
-      // console.log(this.location);
       this.userCoordLat = data['results'][0].geometry.location.lat;
       this.userCoordLng = data['results'][0].geometry.location.lng;
-      console.log(this.userCoordLat);
-      console.log(this.userCoordLng);
       let observe = this._httpService.getHospital(this.location);
       observe.subscribe(response => {
         this.hospital = response;
         console.log(this.hospital);
         for(let i = 0; i < (this.hospital).length; i++){
-          console.log((this.hospital[i].location.coordinates[1]) + ',' + (this.hospital[i].location.coordinates[0]));
           this.hospCoordLat = this.hospital[i].location.coordinates[1];
           this.hospCoordLng = this.hospital[i].location.coordinates[0];
-          console.log(this.hospCoordLat);
-          console.log(this.hospCoordLng);
           let observing = this._httpService.getDistance(this.userCoordLat,this.userCoordLng,this.hospCoordLat,this.hospCoordLng);
           observing.subscribe(dist => {
-            this.distance = dist;
-            console.log(this.distance);
+            console.log(dist);
+            console.log("i: " + i);
+            let distance = dist['rows'][0].elements[0].distance.text;
+            console.log(distance);
+            this.hospital[i]['distance'] = distance;
+            // this.distList.push(distance);
+            console.log(this.hospital);
           })
         }
       })
